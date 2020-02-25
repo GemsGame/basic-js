c0fd6567619c40e3dec563a1e0de4c99fd9d6790
GemsGame <a.masanovets@gmail.com>
2020-02-25 14:10:42 +0300

    feat: add solution cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,7 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+let counter = 0;
+matrix.forEach(element => {
+  element.filter(el => el === "^^"? counter +=1:null);
+});
+return counter;
 };
