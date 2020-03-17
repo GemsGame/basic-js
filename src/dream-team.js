bf983567ac5c37bdf9efcacb5d201d599614591d
GemsGame <a.masanovets@gmail.com>
2020-03-18 01:15:11 +0300

    feat: add dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,6 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    let cleanArray = members.filter((item, index) => typeof item === "string").map(item => item.trim()).map(item => item[0].toUpperCase());
+    return cleanArray.sort().join("");
+  } else return false;
 };
