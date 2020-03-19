710e0adcee926cea79f31832731a5ee61bbfc348
GemsGame <a.masanovets@gmail.com>
2020-03-19 03:48:23 +0300

    feat: add hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,6 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeedPerHour) {
+  let minTurns = Math.pow(2, disksNumber) - 1;
+  let turninSecond = turnsSpeedPerHour / (60 * 60);
+  let minSec = minTurns / turninSecond;
+  return {turns: minTurns,seconds: minSec};
 }
