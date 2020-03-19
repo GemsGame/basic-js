17d5bd15239fbb39bf6091c8f2a02f08df6d5474
GemsGame <a.masanovets@gmail.com>
2020-03-19 03:13:10 +0300

    feat: add season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,20 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined)  {
+    return 'Unable to determine the time of year!';
+  }
+  if(date.hasOwnProperty('getMonth')) {
+    throw new Error();
+  } else {
+    const whatMonth = date.getMonth();
+     if(whatMonth === 11 || whatMonth === 0 || whatMonth === 1) {
+       return 'winter';
+     }
+     if(whatMonth === 4 || whatMonth === 2 || whatMonth === 3) {
+      return "spring";
+    }
+    if(whatMonth === 5 || whatMonth === 6 || whatMonth === 7) {
+      return "summer";
+    } else return 'autumn';
+    
+  } 
 };
