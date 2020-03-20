48f6d088e864b5ab9654ba53fac2f5e3caf02e44
GemsGame <a.masanovets@gmail.com>
2020-03-20 03:40:43 +0300

    feat: add transform beta

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,39 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+if(Array.isArray(arr) === false) { throw new Error;}
+if(arr.length === 0) {return arr;}
+
+let array = arr;
+
+for(let index = 0; index < array.length; index++) {
+
+if(array[index] === '--discard-next') {
+    if(array.length - 1 != index) {
+    array.splice(index + 1, 1);
+    }
+    array.splice(index, 1);
+}
+if(array[index] === '--discard-prev') {
+    if(index != 0) {
+    array.splice(index - 1, 1);
+    }
+    array.splice(index, 1);
+}
+if(array[index] === '--double-next') {
+    if(array.length - 1 != index) {
+    array.splice(index + 1, 0, item[index + 1]);
+    array.splice(index, 1);
+    }
+}
+if(array[index] === '--double-prev') {
+    if(index != 0) {
+    array.splice(index - 1, 0, item[index - 1]);
+
+    }
+    array.splice(index, 1);
+}
+
+}
+
+return array;
+
 };
